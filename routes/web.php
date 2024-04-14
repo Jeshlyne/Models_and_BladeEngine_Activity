cf256560dda481c769ca757e2aae6e8c5eff8686
unknown <abaloslyne@gmail.com>
2024-04-15 00:18:17 +0800

    Created new files

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
